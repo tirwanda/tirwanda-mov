faa2d2ae998181805e007f3c3bcf3273acc23abd
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-01-25 11:44:52 +0700

    initial commit

---
+++ app/src/main/java/com/tirwanda/bwa_mov/onBoarding/OnBoardingOneActivity.kt
@@ -0,0 +1,12 @@
+package com.tirwanda.bwa_mov.onBoarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tirwanda.bwa_mov.R
+
+class OnBoardingOneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_one)
+    }
+}
