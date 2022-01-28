a68069eb379a1c905a976028c7ee447627c9e3e9
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-01-28 10:54:24 +0700

    Create Kotlin File for Sign Up

---
+++ app/src/main/java/com/tirwanda/bwa_mov/SignUpPhotoProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.tirwanda.bwa_mov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpPhotoProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_photo_profile)
+    }
+}
