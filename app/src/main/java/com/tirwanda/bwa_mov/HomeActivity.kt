df150d8945dcec59074416c983c2e7100da8289d
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-01-29 19:20:03 +0700

    Create Activity Home

---
+++ app/src/main/java/com/tirwanda/bwa_mov/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.tirwanda.bwa_mov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
