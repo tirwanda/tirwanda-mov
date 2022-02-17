e6f01435e83b40963a86c478c5c929a59ae79997
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-02-17 16:32:17 +0700

    Create Checkout Page

--- app/src/main/java/com/tirwanda/bwa_mov/sign/SignInActivity.kt
+++ app/src/main/java/com/tirwanda/bwa_mov/sign/SignInActivity.kt
@@ -1,11 +1,12 @@
-package com.tirwanda.bwa_mov
+package com.tirwanda.bwa_mov.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tirwanda.bwa_mov.R
 
-class SignUpPhotoProfileActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up_photo_profile)
+        setContentView(R.layout.activity_sign_in)
     }
 }
--- app/src/main/java/com/tirwanda/bwa_mov/sign/SignUpActivity.kt
+++ app/src/main/java/com/tirwanda/bwa_mov/sign/SignUpActivity.kt
@@ -1,7 +1,8 @@
-package com.tirwanda.bwa_mov
+package com.tirwanda.bwa_mov.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tirwanda.bwa_mov.R
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
