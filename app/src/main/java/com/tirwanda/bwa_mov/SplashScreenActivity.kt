b6938dab3bb1e35e97bb84dcc9d05d05fbef19c3
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-02-18 20:54:46 +0700

    Activate Splash Screen

--- app/src/main/java/com/tirwanda/bwa_mov/SplashScreenActivity.kt
+++ app/src/main/java/com/tirwanda/bwa_mov/SplashScreenActivity.kt
@@ -1,11 +1,22 @@
 package com.tirwanda.bwa_mov
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.tirwanda.bwa_mov.onBoarding.OnBoardingOneActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this@SplashScreenActivity, OnBoardingOneActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
     }
 }
