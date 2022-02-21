02e7aceb9594396f10901101f0d7d66856ae29ed
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-02-21 20:57:38 +0700

    Connecting App to Firebase

--- app/src/main/java/com/tirwanda/bwa_mov/sign/SignInActivity.kt
+++ app/src/main/java/com/tirwanda/bwa_mov/sign/SignInActivity.kt
@@ -3,10 +3,21 @@ package com.tirwanda.bwa_mov.sign
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tirwanda.bwa_mov.R
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import com.google.firebase.database.DatabaseReference
+
+import com.google.firebase.database.FirebaseDatabase
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        btnSignIn.setOnClickListener {
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference("message")
+
+            myRef.setValue("Hello, World!")
+        }
     }
 }
