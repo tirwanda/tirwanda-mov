02792e6dc8b40c7fe620d90c98a6d99ccbf0e857
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-02-28 21:23:35 +0700

    Create Login with Firebase

---
+++ app/src/main/java/com/tirwanda/bwa_mov/entity/User.kt
@@ -0,0 +1,11 @@
+package com.tirwanda.bwa_mov.entity
+
+class User {
+
+    var email: String ?= ""
+    var username: String ?= ""
+    var nama: String ?= ""
+    var password: String ?= ""
+    var saldo: String ?= ""
+    var url: String ?= ""
+}
--- app/src/main/java/com/tirwanda/bwa_mov/sign/SignInActivity.kt
+++ app/src/main/java/com/tirwanda/bwa_mov/sign/SignInActivity.kt
@@ -1,23 +1,70 @@
 package com.tirwanda.bwa_mov.sign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.*
 import com.tirwanda.bwa_mov.R
 import kotlinx.android.synthetic.main.activity_sign_in.*
-import com.google.firebase.database.DatabaseReference
 
-import com.google.firebase.database.FirebaseDatabase
+import com.tirwanda.bwa_mov.HomeActivity
+import com.tirwanda.bwa_mov.entity.User
 
 class SignInActivity : AppCompatActivity() {
+    lateinit var iUsername:String
+    lateinit var iPassword:String
+
+    lateinit var mDatabase: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        btnSignIn.setOnClickListener {
-            val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("message")
+        mDatabase = FirebaseDatabase.getInstance().getReference("User")
 
-            myRef.setValue("Hello, World!")
+        btnSignIn.setOnClickListener {
+            iUsername = etUsername.text.toString()
+            iPassword = etPassword.text.toString()
+
+            when {
+                iUsername == "" -> {
+                    etUsername.error = "Username is required"
+                    etUsername.requestFocus()
+                }
+                iPassword == "" -> {
+                    etPassword.error = "Password is required"
+                    etPassword.requestFocus()
+                }
+                else -> {
+                    pushLogin(iUsername, iPassword)
+                }
+            }
         }
+    }
+
+    private fun pushLogin(username: String, password: String) {
+        mDatabase.child(username).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val user = dataSnapshot.getValue(User::class.java)
+
+                if (user == null) {
+                    Toast.makeText(this@SignInActivity, "Username or password is invalid",
+                        Toast.LENGTH_LONG).show()
+                } else {
+                    if (user.password == password) {
+                        val intent = Intent(this@SignInActivity, HomeActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this@SignInActivity, "Username or password is invalid",
+                            Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(this@SignInActivity, databaseError.message, Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
